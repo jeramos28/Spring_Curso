dce610ebcb878fcbaa47192ea13c97f010b3612e
jeramos28 <96jeramos@gmail.com>
2023-09-21 15:52:10 -0400

    creando listado de usuarios y mostrandolos mediante el js y html de manera dinamica

--- curso/src/main/resources/static/js/usuarios.js
+++ curso/src/main/resources/static/js/usuarios.js
@@ -1,5 +1,25 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-
+    cargarUsuarios();
   $('#usuarios').DataTable();
 });
+
+async function cargarUsuarios(){
+
+    const request = await fetch('usuarios',{
+      method: 'GET',
+          headers:{
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+  }
+});
+  const usuarios = await request.json();
+
+  let listadohtml = '';
+  for (let usuario of usuarios){
+      let usuariohtml = '<tr> <td>'+ usuario.nombre +'</td> <td>'+ usuario.apellido +'</td> <td>'+ usuario.email +'</td> <td><a href="#" class="btn btn-danger btclassNamecle btn-sm"> <i class="fas fa-trash"></iclassName </a></td> </tr>'
+      listadohtml += usuariohtml;
+  }
+
+document.querySelector('#usuarios tbody').outerHTML= listadohtml;
+}
